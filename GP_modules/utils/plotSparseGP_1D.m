ee087b80f7e20b9f3acade0b22132ca67c2b64c2
TangKai-HIT <1292737216@qq.com>
2023-10-25 23:01:31 +0800

    add more input options

--- GP_modules/utils/plotSparseGP_1D.m
+++ GP_modules/utils/plotSparseGP_1D.m
@@ -1,8 +1,12 @@
-function [sampleStates, sampleId] = plotSparseGP_1D(ax, GPTrajSparse)
+function [sampleStates, sampleId] = plotSparseGP_1D(ax, GPTrajSparse, dim, N, varScale)
 %PLOTSPARSEGP_1D 1-D trajectory plot util for GPTrajSparse objects
 %   GPTrajSparse: derived-class from GPTrajSparseBase
+%   varScale: scale scalar of state variance plot
 
-N = 500;
+if ~exist("varScale", "var")
+    varScale = 2E1;
+end
+
 sampleId = linspace(GPTrajSparse.supportId(1), GPTrajSparse.supportId(end), N);
 sampleStates = zeros(N, GPTrajSparse.dim);
 sampleVars = zeros(GPTrajSparse.dim, GPTrajSparse.dim, N);
@@ -14,18 +18,18 @@ end
 hold(ax, "on");
 % plot variance patch
 patch(ax, [sampleId, sampleId(end:-1:1)], ...
-		[sampleStates(:, 1)+squeeze(abs(sampleVars(1, 1, :)).^.5)*2E1; sampleStates(end:-1:1, 1)-squeeze(abs(sampleVars(1, 1, end:-1:1)).^.5)*2E1], ...
+		[sampleStates(:, dim)+squeeze(abs(sampleVars(dim, dim, :)).^.5)*varScale; sampleStates(end:-1:1, dim)-squeeze(abs(sampleVars(dim, dim, end:-1:1)).^.5)*varScale], ...
 		[1 .8 .8],'edgecolor','none'); %added abs to avoid negative value around 0 (numerical problem) 
 
 % plot expectation trajectory
-plot(ax, sampleId, sampleStates(:, 1), '-r', 'LineWidth', 1.5);
-scatter(ax, GPTrajSparse.supportId, GPTrajSparse.supportPts(:, 1), 'black', 'filled' ,'o', SizeData=30);
+plot(ax, sampleId, sampleStates(:, dim), '-r', 'LineWidth', 1.5);
+scatter(ax, GPTrajSparse.supportId, GPTrajSparse.supportPts(:, dim), 'black', 'filled' ,'o', SizeData=30);
 
-min_y = min(sampleStates(:, 1));
-max_y = max(sampleStates(:, 1));
+min_y = min(sampleStates(:, dim));
+max_y = max(sampleStates(:, dim));
 mid_y = (min_y + max_y)/2;
 gap = max_y - min_y;
-ylim([min_y-gap/2, max_y+gap/2]);
+ylim([mid_y-3*gap/2, mid_y+3*gap/2]);
 grid on;
 
 end
