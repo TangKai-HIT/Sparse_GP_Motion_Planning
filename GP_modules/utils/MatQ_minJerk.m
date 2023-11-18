b0106f7c29542ab4b1fe98c362efa621a88dfd4c
TangKai-HIT <1292737216@qq.com>
2023-11-18 22:08:04 +0800

    debugged, found a mistake of the formula in original paper

--- GP_modules/utils/MatQ_minJerk.m
+++ GP_modules/utils/MatQ_minJerk.m
@@ -4,7 +4,7 @@ function Q = MatQ_minJerk(a, b, Q_c)
 %   此处显示详细说明
 
 delta = b-a;
-Q = [1/2*delta^5*Q_c,   1/8*delta^4*Q_c,    1/6*delta^3*Q_c;
+Q = [1/20*delta^5*Q_c,   1/8*delta^4*Q_c,    1/6*delta^3*Q_c;
         1/8*delta^4*Q_c,    1/3*delta^3*Q_c,    1/2*delta^2*Q_c;
         1/6*delta^3*Q_c,    1/2*delta^2*Q_c,    delta*Q_c];
 end
